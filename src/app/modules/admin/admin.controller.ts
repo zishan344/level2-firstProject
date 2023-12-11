bf03ef1e5dc59db1188737b44198fbbeee904f28
zishan344 <zishanahmed344@gmail.com>
2023-12-11 09:36:47 +0600

    added admin controller

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from 'express';
+
+import httpStatus, { OK } from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AdminServices } from './admin.service';
+
+const getAllAdmin = catchAsync(async (req: Request, res: Response) => {
+  const result = await AdminServices.getAllAdminFromDB(req.query);
+  sendResponse(res, {
+    statusCode: OK,
+    success: true,
+    message: 'admin are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAdmin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      const result = await AdminServices.getSingleAdminFromDB(id);
+      sendResponse(res, {
+        statusCode: OK,
+        success: true,
+        message: 'admin is retrieved successfully',
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+);
+const updateAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { admin } = req.body;
+
+  const result = await AdminServices.updateAdminIntoDB(id, admin);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'admin is updated successfully',
+    data: result,
+  });
+});
+
+const deleteAdmin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      const result = await AdminServices.deleteAdminFromDB(id);
+      sendResponse(res, {
+        statusCode: OK,
+        success: true,
+        message: 'admin is deleted successfully',
+        data: result,
+      });
+    } catch (err: any) {
+      next(err);
+    }
+  },
+);
+
+export const facultyControllers = {
+  getAllAdmin,
+  getSingleAdmin,
+  updateAdmin,
+  deleteAdmin,
+};
